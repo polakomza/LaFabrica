6611d9731253fce1082801ece50cf1ffa96bee9e
Brunoburgoa <brunocsir515@gmail.com>
2024-06-17 19:56:44 -0300

    hola

--- src/main/java/com/mycompany/lafabrica/LaFabrica.java
+++ src/main/java/com/mycompany/lafabrica/LaFabrica.java
@@ -5,6 +5,7 @@ package com.mycompany.lafabrica;
 public class LaFabrica {
 
     public static void main(String[] args) {
-        
+        System.out.println("(.)(.)");
+                
     }
 }
