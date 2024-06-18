89cfec8f818f43a0ef07a0054f5cf2e0ccc7d178
eduardomaximiliano <eduardomaximiliano12@hotmail.com>
2024-06-17 22:12:00 -0300

    Arrays en MateriaPrima y OrdenProduccion

--- src/main/java/com/mycompany/lafabrica/LaFabrica.java
+++ src/main/java/com/mycompany/lafabrica/LaFabrica.java
@@ -1,10 +1,18 @@
-
 package com.mycompany.lafabrica;
 
-
+import java.util.ArrayList;
+//import javax.swing.JOptionPane;
 public class LaFabrica {
 
     public static void main(String[] args) {
-        
+
+        ArrayList<MateriaPrima> mat_prima = MateriaPrima.crearListaProductos();
+
+        for (MateriaPrima i : mat_prima) {
+            System.out.println(i);
+            //JOptionPane.showInputDialog(null,"ñoquis");
+        }
+
     }
 }
+
--- src/main/java/com/mycompany/lafabrica/OrdenProduccion.java
+++ src/main/java/com/mycompany/lafabrica/OrdenProduccion.java
@@ -1,20 +1,46 @@
-
 package com.mycompany.lafabrica;
 
+import java.util.ArrayList;
+
+/*Las órdenes de producción indican
+la cantidad de unidades de un 
+determinado producto que hay
+ que fabricar. */
 
 public class OrdenProduccion {
-    public String producto_a_fabricar;
-    public boolean cumplida;
-    public int cantidad;
-    public Producto producto;
-
+    private String producto_a_fabricar;
+    private boolean cumplida;
+    private int cantidad;
+    private Producto producto;
+    private String codigo;
+    public OrdenProduccion() {
+    }
+    
     public OrdenProduccion(String producto_a_fabricar, boolean cumplida, int cantidad, Producto producto) {
         this.producto_a_fabricar = producto_a_fabricar;
         this.cumplida = cumplida;
         this.cantidad = cantidad;
         this.producto = producto;
     }
+
+    public OrdenProduccion(String codigo, String producto_a_fabricar, int cantidad) {
+        this.producto_a_fabricar = producto_a_fabricar;
+        this.cantidad = cantidad;
+        this.codigo = codigo;
+    }
     
+    
+    public static ArrayList<OrdenProduccion> mostrarProductos(){
+        ArrayList<OrdenProduccion> mostrar = new ArrayList<>();
+        mostrar.add(new OrdenProduccion("100", "Gnoquis", 20));
+        mostrar.add(new OrdenProduccion("101", "Fideos Rellenos", 30));
+        mostrar.add(new OrdenProduccion("102", "Spaggetti", 50));
+        mostrar.add(new OrdenProduccion("103", "Ravioles", 15));
+        mostrar.add(new OrdenProduccion("104", "Sorrentinos", 28));
+        mostrar.add(new OrdenProduccion("105", "Capeletinni", 28));
+        
+        return mostrar;
+    }
     public boolean verificar_cumplido(){
         //recibira por parametro lo que retorne de crear producto
         return false;
